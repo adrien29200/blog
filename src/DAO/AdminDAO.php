a20ada147cca5263b5ad2db7c9300c93c592b9cd
Adrien <adrien.guillerm01@gmail.com>
2020-04-25 09:57:21 +0200

    ajout adminDAO

---
+++ src/DAO/AdminDAO.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\src\DAO;
+
+use App\config\Parameter;
+
+class AdminDAO extends DAO
+{
+    public function login(Parameter $post)
+    {
+        $sql = 'SELECT admin.pseudo, admin.password FROM admin';
+        $data = $this->createQuery($sql, [$post->get('pseudo')]);
+        $result = $data->fetch();
+        $isPasswordValid = password_verify($post->get('password'), $result['password']);
+        return [
+            'result' => $result,
+            'isPasswordValid' => $isPasswordValid
+        ];
+    }
+
+    public function updatePassword(Parameter $post, $pseudo)
+    {
+        $sql = 'UPDATE admin SET password = ?';
+        $this->createQuery($sql, [password_hash($post->get('password'), PASSWORD_BCRYPT), $pseudo]);
+    }
+
+}
