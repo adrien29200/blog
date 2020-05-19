451d2fa8c200f2dfe9e657971dcb10f7ccf3d40e
Adrien <adrien.guillerm01@gmail.com>
2020-05-19 17:06:20 +0200

    modification gitignore

--- config/dev.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const HOST = 'adrienguqmdb.mysql.db';
-const DB_NAME = 'adrienguqmdb';
-const CHARSET = 'utf8';
-const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
-const DB_USER = 'adrienguqmdb';
-const DB_PASS = 'Spartan117';
